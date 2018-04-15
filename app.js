246bfe83762f2863427d2578c23f677042067615
Aleix Sanchis <aleixsanchis@hotmail.com>
2018-04-15 23:53:49 +0200

    Ja funciona

--- app.js
+++ app.js
@@ -3,12 +3,36 @@ var app = express();
 var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
-
+var child_process = require('child_process');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+var uploadFolder = path.join(__dirname, '/uploads/');
+app.get('/download', function(req, res){
+  child_process.exec("tar -cvf uploads.tar *.hex", {cwd: uploadFolder}, function(err, stdout, stderr){
+    if(err){
+      console.log(err);
+      res.send("Borro els codis quan ja shan baixat. Torna a provar i fes un F5 bueno");
+    }
+    else{
+      res.download(path.join(__dirname, 'uploads/uploads.tar'), "codis.tar");
+    }
+  });
+
+  res.on('finish', function(){
+    child_process.exec("rm *.hex *.tar", {cwd: uploadFolder}, function(err, stdout, stderr){
+      if(err){
+        console.log(err);
+      }
+      else{
+  
+      }
+    })
+  })
+  
+})
 
 app.post('/upload', function(req, res){
 
@@ -25,7 +49,6 @@ app.post('/upload', function(req, res){
   // rename it to it's orignal name
   form.on('file', function(field, file) {
     fs.rename(file.path, path.join(form.uploadDir, file.name));
-    console.log(file, file.path, file.name);
   });
 
   // log any errors that occur
@@ -35,7 +58,52 @@ app.post('/upload', function(req, res){
 
   // once all the files have been uploaded, send a response to the client
   form.on('end', function() {
-    res.end('success');
+    fs.readdir(form.uploadDir, (err, files) => {
+      files.forEach(file => {
+        var file_sans_extension = file.split(".")[0];
+        var file_extension = file.split(".")[1];
+        console.log(file_sans_extension, file_extension);
+        if(file_extension == 's'){
+          child_process.exec('sisa-as '+path.join(form.uploadDir, file)+" -o "+file_sans_extension+".out", {cwd: form.uploadDir}
+            , function(err, stdout, stderr){
+            if(err){
+              res.end("liada");
+              console.log(err);
+            }
+            else{
+              child_process.exec('sisa-objcopy -O binary -j .text ' + file_sans_extension+".out "+ file_sans_extension+".dump", {cwd: form.uploadDir},
+                function(err, stdout, stderr){
+                if(err){
+                  console.log(err);
+                }
+                else{
+                  child_process.exec('od -An -w2 -x '+file_sans_extension+".dump" + " > "+file_sans_extension+".hex", {cwd: form.uploadDir},function(err, stdout, stderr){
+                    if(err){
+                      console.log(err);
+                    }
+                    else{
+                      child_process.exec("sed -i -e 's/ //' "+file_sans_extension+".hex", {cwd: form.uploadDir}, function(err, stdout, stderr){
+                        if(err){
+                          console.log(err);
+                        }
+                        else{
+                          child_process.exec("rm "+file_sans_extension+".dump "+file_sans_extension+".out "+file_sans_extension+".s ", {cwd: form.uploadDir}, function(err, stdout, stderr){
+                            if(err){
+                              console.log(err);
+                            }
+                          })
+                        }
+                      });
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    })
+    res.send("success");
   });
 
   // parse the incoming request containing the form data
--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -21,43 +21,30 @@ $('#upload-input').on('change', function(){
       formData.append('uploads[]', file, file.name);
     }
 
-    $.ajax({
-      url: '/upload',
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function(data){
-          console.log('upload successful!\n' + data);
-      },
-      xhr: function() {
-        // create an XMLHttpRequest
-        var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
+    // listen to the 'progress' event
+    xhr.upload.addEventListener('progress', function(evt) {
 
-        // listen to the 'progress' event
-        xhr.upload.addEventListener('progress', function(evt) {
+      if (evt.lengthComputable) {
+        // calculate the percentage of upload completed
+        var percentComplete = evt.loaded / evt.total;
+        percentComplete = parseInt(percentComplete * 100);
 
-          if (evt.lengthComputable) {
-            // calculate the percentage of upload completed
-            var percentComplete = evt.loaded / evt.total;
-            percentComplete = parseInt(percentComplete * 100);
+        // update the Bootstrap progress bar with the new percentage
+        $('.progress-bar').text(percentComplete + '%');
+        $('.progress-bar').width(percentComplete + '%');
 
-            // update the Bootstrap progress bar with the new percentage
-            $('.progress-bar').text(percentComplete + '%');
-            $('.progress-bar').width(percentComplete + '%');
+        // once the upload reaches 100%, set the progress bar text to done
+        if (percentComplete === 100) {
+          $('.progress-bar').html('Done');
+          $('#download-btn').show();
+        }
 
-            // once the upload reaches 100%, set the progress bar text to done
-            if (percentComplete === 100) {
-              $('.progress-bar').html('Done');
-            }
-
-          }
-
-        }, false);
-
-        return xhr;
       }
-    });
 
+    }, false);
+
+    xhr.open("POST", '/upload');
+    xhr.send(formData);
   }
 });
